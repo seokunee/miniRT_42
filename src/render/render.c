8c83a91a2ea9e76fc70ed82beed98e89f2d35475
Chanwoong1 <jcw7468@gmail.com>
2023-01-16 21:43:52 +0900

    feat: info 구조체 받아서 픽셀 생성하는 부분 구현 시작
    픽셀 하나하나씩 4차원 배열으로 생성해서 그려주면 될것같음

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: chanwjeo <chanwjeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 16:04:42 by chanwjeo          #+#    #+#             */
-/*   Updated: 2023/01/16 17:23:34 by chanwjeo         ###   ########.fr       */
+/*   Updated: 2023/01/16 21:42:42 by chanwjeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 void	draw_pixel(t_info *info, t_vec3 *pixel)
 {
 	t_ray	*pixel_ray;
+	t_vec4	*mlx_pixel;
 
+	pixel_ray = ft_malloc(sizeof(t_ray));
 	pixel_ray->start = pixel;
-	pixel_ray->dir = 
+	pixel_ray->dir = normalize_3d_vector(v_sub(pixel, info->t_c->coor));
+	mlx_pixel = create_4d_vec_input_3d_vector(clamp_3d(trace_ray(pixel_ray, 5), 0.0f, 1.0f), 1.0f);
+	// draw;
+	free_ray(pixel_ray);
+	free(mlx_pixel);
 }
 
 void	render(t_info *info)
@@ -28,10 +34,10 @@ void	render(t_info *info)
 	t_vec3	*pixel;
 
 	h = -1;
-	while (++h < info->window->height)
+	while (++h < info->hei)
 	{
 		w = -1;
-		while (++w < info->window->width)
+		while (++w < info->wid)
 		{
 			pos = create_2d_vec((float)w, (float)h);
 			pixel = transform_screen_to_world(info, pos);
