7d6ed24b6fc6f66bb88195520fb320782cccf865
chanwoong1 <jcw7468@gmail.com>
2023-01-16 09:32:51 +0900

    feat: Implement find_closest_collision (임시)
    파싱에 따라 구조 변경되어야 함

--- src/render/ray_tracer.c
+++ src/render/ray_tracer.c
@@ -6,7 +6,7 @@
 /*   By: chanwjeo <chanwjeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 00:10:23 by chanwjeo          #+#    #+#             */
-/*   Updated: 2023/01/16 00:15:49 by chanwjeo         ###   ########.fr       */
+/*   Updated: 2023/01/16 09:31:53 by chanwjeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,20 @@ t_hit	*find_closest_collision(t_ray *ray)
 	float	closest_d;
 	t_hit	*closest_hit;
 	t_hit	*tmp_hit
-	int		i;
+	// t_list	*head;	필요하다면 사용
 
-	closest_hit = ft_malloc(sizeof(t_hit));
+	closest_hit = NULL;
 	closest_d = 2147483647.0f
-	i = -1;
-	while (++i < ray->num_of_objs)
+	while (end_of_tlist)
 	{
-		tmp_hit = 
-		
-	}
-	for (int l = 0; l < objects.size(); l++)
-	{
-		auto hit = objects[l]->CheckRayCollision(ray);
-
-		if (hit.d >= 0.0f)
+		tmp_hit = check_ray_collision(t_list->content);
+		if (tmp_hit->d >= 0.0f && tmp_hit->d < closest_d)
 		{
-			if (hit.d < closestD)
-			{
-				closestD = hit.d;
-				closestHit = hit;
-				closestHit.obj = objects[l];
-
-				// 텍스춰 좌표
-				closestHit.uv = hit.uv;
-			}
+			if (closest_hit)
+				free(closest_hit);
+			closest_hit = tmp_hit;
 		}
+		// t_list = t_list->next;
 	}
 }
+
