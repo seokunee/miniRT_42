77181f446c218002c1d07d956bccfef05147fb72
sunhwang <sunhwang@student.42seoul.kr>
2023-01-16 14:49:14 +0900

    refacotr : ft_malloc Norm 맞춤

--- src/libft/src/ft_malloc.c
+++ src/libft/src/ft_malloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_malloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chanwjeo <chanwjeo@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: sunhwang <sunhwang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 15:23:04 by chanwjeo          #+#    #+#             */
-/*   Updated: 2023/01/11 16:15:06 by chanwjeo         ###   ########.fr       */
+/*   Updated: 2023/01/16 14:35:54 by sunhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 
 void	*ft_malloc(size_t size)
 {
-	void *p;
+	void	*p;
 
 	p = ft_calloc(1, size);
 	if (!p)
 		exit(1);
-	return p;
+	return (p);
 }
