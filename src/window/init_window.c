a025ad39cd5a1e06800e242baa754b43c1084159
sunhwang <sunhwang@student.42seoul.kr>
2023-01-17 14:46:47 +0900

    refactor : sizex -> width / sizey -> height 변경

--- src/window/init_window.c
+++ src/window/init_window.c
@@ -6,11 +6,12 @@
 /*   By: sunhwang <sunhwang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 16:33:26 by sunhwang          #+#    #+#             */
-/*   Updated: 2023/01/16 17:59:47 by sunhwang         ###   ########.fr       */
+/*   Updated: 2023/01/17 14:16:41 by sunhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "window.h"
+#include "stdio.h" // TODO 지우기
 
 void	init_window(t_window *win)
 {
@@ -20,11 +21,13 @@ void	init_window(t_window *win)
 	w = *win;
 	d = w.data;
 	w.mlx = mlx_init();
-	mlx_get_screen_size(w.mlx, &w.sizex, &w.sizey);
-	w.sizex /= 2;
-	w.sizey /= 2;
-	w.mlx_win = mlx_new_window(w.mlx, w.sizex, w.sizey, TITLE);
-	d.img = mlx_new_image(w.mlx, w.sizex, w.sizey);
+	mlx_get_screen_size(w.mlx, &w.width, &w.height);
+	printf("width :%d heigth: %d\n", w.width, w.height);
+	w.width /= 2;
+	w.height /= 2;
+	printf("width :%d heigth: %d\n", w.width, w.height);
+	w.mlx_win = mlx_new_window(w.mlx, w.width, w.height, TITLE);
+	d.img = mlx_new_image(w.mlx, w.width, w.height);
 	d.addr = mlx_get_data_addr(d.img, &d.bits_per_pixel, &d.line_length, \
 	&d.endian);
 	w.data = d;
