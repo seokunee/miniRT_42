929225692a5cbef3e8c20a152ee343f0dcb60735
sunhwang <sunhwang@student.42seoul.kr>
2023-01-18 12:36:08 +0900

    feat : draw_image 파일 생성(pixel에 색상 넣는 작업)

--- includes/window.h
+++ includes/window.h
@@ -6,7 +6,7 @@
 /*   By: sunhwang <sunhwang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 14:56:12 by sunhwang          #+#    #+#             */
-/*   Updated: 2023/01/17 14:16:43 by sunhwang         ###   ########.fr       */
+/*   Updated: 2023/01/17 16:52:56 by sunhwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,8 @@ void	binding_events(t_window *win);
 int		binding_key_events(int key, t_window *win);
 int		binding_mouse_events(int button, int x, int y, t_window *win);
 int		exit_event(t_window *win);
+// TODO trgb 함수는 static 함수화 생각할 것
+int		create_trgb(unsigned char t, unsigned char r, unsigned char g, unsigned char b);
+void	put_pixel(t_data *data, int x, int y, int color);
 
 #endif
---
+++ src/window/draw_image.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_image.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhwang <sunhwang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 15:08:40 by sunhwang          #+#    #+#             */
+/*   Updated: 2023/01/17 17:10:44 by sunhwang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+int	create_trgb(unsigned char t, unsigned char r, unsigned char g, \
+unsigned char b)
+{
+	return (*(int *)(unsigned char [4]){b, g, r, t});
+}
+
+static int	get_offset(t_data *data, int x, int y)
+{
+	const int	line_length = data->line_length;
+	const int	bits_per_pixel = data->bits_per_pixel;
+	const int	offset = (y * line_length + x * (bits_per_pixel / 8));
+
+	return (offset);
+}
+
+void	put_pixel(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + get_offset(data, x, y);
+	*(unsigned int *)dst = color;
+}
