6b440f479cedd5822ffa1f90d5b7a552ce139d26
chanwoong1 <jcw7468@gmail.com>
2023-02-03 14:57:16 +0900

    fix: 원뿔은 objs 카운트 안되던 현상 수정

--- src/parse/check_option_count.c
+++ src/parse/check_option_count.c
@@ -6,7 +6,7 @@
 /*   By: chanwjeo <chanwjeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 14:36:33 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/02/01 12:07:06 by chanwjeo         ###   ########.fr       */
+/*   Updated: 2023/02/03 14:10:37 by chanwjeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ void	check_option_count(char *s, t_num_ele *check_count)
 	else if (ft_strncmp(s, "pl", 3) == 0)
 		check_count->objs_count++;
 	else if (ft_strncmp(s, "cy", 3) == 0)
+		check_count->objs_count++;
+	else if (ft_strncmp(s, "cn", 3) == 0)
 		check_count->objs_count++;
 	if (check_count->a_count > 1 || check_count->c_count > 1)
 		error_exit("Invalid number of options");
