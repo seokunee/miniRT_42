10548ca4462f0703325c248593a54b362b44f8ef
seokunee <xocoolree@gmail.com>
2023-02-14 14:52:23 +0900

    refactoring : 쓸데없는 나눗셈 제거

--- src/window/init_window.c
+++ src/window/init_window.c
@@ -16,10 +16,8 @@
 void	init_window(t_window *win)
 {
 	win->mlx = mlx_init();
-	win->width = 800;
-	win->height = 800;
-	win->width /= 2;
-	win->height /= 2;
+	win->width = 600;
+	win->height = 600;
 	win->mlx_win = mlx_new_window(win->mlx, win->width, win->height, TITLE);
 	ft_memset(&win->terminal, 0, sizeof(win->terminal));
 }
